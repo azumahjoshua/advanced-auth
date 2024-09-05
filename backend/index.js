bbfed9e3c51f50e03eb2b7f3abc0848c7b5aec06
azumahjoshua <joshuaazumah1086@gmail.com>
2024-09-05 12:21:17 +0000

    middleware complete"

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connetDB.js";
 dotenv.config();
 import router from "./routes/auth.route.js";
@@ -8,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
-
+app.use(cookieParser())
 app.use("/api/auth",router)
 app.listen(PORT,()=>{
     connectDB();
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import {resetPassword, forgotPassword, verifyEmail, login, logout,signup } from "../controllers/auth.controller.js";
+import {resetPassword, forgotPassword, verifyEmail, login, logout,signup, checkAuth } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.get('/check-auth',verifyToken,checkAuth)
 
 router.post("/signup", signup)
 
